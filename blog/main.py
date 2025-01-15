1c13bc6901147e7dec954e9f4cc545afc294f243
“Negin <n.haghighy@gmail.com>
2025-01-15 13:54:54 +0330

    HTTP responce and status codes

--- blog/main.py
+++ blog/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI , Depends
+from fastapi import FastAPI , Depends, status, Response, HTTPException
 from . import schemas, models
 from .database import engine, SessionLocal
 from sqlalchemy.orm import Session
@@ -13,6 +13,24 @@ def get_db():
     finally:
         db.close()
 
-@app.post('/blog')
+@app.post('/blog',status_code=status.HTTP_201_CREATED)
 def create(request:schemas.Blog, db : Session = Depends(get_db)):
-    new_blog 
+    new_blog = models.Blog(title=request.title, body=request.body)
+    db.add(new_blog)
+    db.commit()
+    db.refresh(new_blog)
+    return new_blog
+
+@app.get('/blog')
+def all(db:Session=Depends(get_db)):
+    blogs=db.query(models.Blog).all()
+    return blogs
+
+@app.get('/blog/{id}', status_code=status.HTTP_200_OK)
+def show(id, responce:Response, db:Session=Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id==id).first()
+    if not blog:
+        # responce.status_code = status.HTTP_404_NOT_FOUND
+        # return {'detail': f"Blog with the id {id} ins not available!"}
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Blog with the id {id} ins not available!")
+    return blog
