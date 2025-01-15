a42065de12860ac7063d8f95f29ce8984f789c75
“Negin <n.haghighy@gmail.com>
2025-01-15 14:57:47 +0330

    Responce model (Serializer)

--- blog/main.py
+++ blog/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI , Depends, status, Response, HTTPException
 from . import schemas, models
 from .database import engine, SessionLocal
 from sqlalchemy.orm import Session
+from typing import List
 
 app = FastAPI()
 models.Base.metadata.create_all(engine)
@@ -21,12 +22,12 @@ def create(request:schemas.Blog, db : Session = Depends(get_db)):
     db.refresh(new_blog)
     return new_blog
 
-@app.get('/blog')
+@app.get('/blog', response_model=List[schemas.ShowBlog])
 def all(db:Session=Depends(get_db)):
     blogs=db.query(models.Blog).all()
     return blogs
 
-@app.get('/blog/{id}', status_code=status.HTTP_200_OK)
+@app.get('/blog/{id}', status_code=status.HTTP_200_OK, response_model=schemas.ShowBlog)
 def show(id, responce:Response, db:Session=Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id==id).first()
     if not blog:
@@ -37,12 +38,20 @@ def show(id, responce:Response, db:Session=Depends(get_db)):
 
 @app.delete('/blog/{id}', status_code=status.HTTP_204_NO_CONTENT)
 def destroy(id, db:Session=Depends(get_db)):
-    db.query(models.Blog).filter(models.Blog.id==id).delete(synchronize_session=False)
+    blog = db.query(models.Blog).filter(models.Blog.id==id)
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Blog with id {id} not found!")
+    blog.delete(synchronize_session=False)
     db.commit()
     return 'done'
 
 @app.put('/blog/{id}', status_code=status.HTTP_202_ACCEPTED)
 def update(id, request:schemas.Blog, db:Session=Depends(get_db)):
-    db.query(models.Blog).filter(models.Blog.id==id).update(request.dict())
+    blog = db.query(models.Blog).filter(models.Blog.id==id)
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Blog with id {id} not found!")
+    blog.update(request.dict())
     db.commit()
     return 'updated'
+
+# @app
